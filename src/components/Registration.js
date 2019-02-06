09600f6bfd64d99c7f931879f93c565b1e95e53f
DevinM6 <devin8821@gmail.com>
2019-02-06 12:57:06 -0500

    Image upload progress 🤢

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -3,9 +3,7 @@ import PropTypes from 'prop-types';
 import Dimensions from 'Dimensions';
 import { Avatar } from 'react-native-elements';
 import {StyleSheet,View,TextInput, TouchableOpacity, Text, StatusBar} from 'react-native';
-
-
-
+import { ImagePicker, Permissions } from 'expo';
 import{Container, Content,Header,Form,Input,Item,Button, Label} from 'native-base';
 
 import firebase from "firebase";
@@ -17,48 +15,38 @@ var config = {
   storageBucket: "studyfsu-74a19.appspot.com",
 };
 
-
 var auth = firebase.auth();
 var database = firebase.database();
+var firestore = firebase.storage()
 
 export default class Registration extends React.Component {
-
   constructor(props){
     super(props)
-
     this.state = ({
       avi: null,
       fullname: null,
       email: null,
       password: null,
-      passwordConfirm: null
+      passwordConfirm: null,
+      currentID: null
     })
   }
 
-
-
-async editAvi(){
-    console.log('wtf')
-       var status = Expo.Permissions.askAsync(Expo.Permissions.CAMERA_ROLL);
-       await Expo.ImagePicker.launchImageLibraryAsync({
-
+editAvi = async () => {
+      await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
       });
-    if (!status.cancelled){
-        this.setState({ avi: status.uri });
-        console.log(avi)
-      }
-
+      if (!cancelled) {
+        this.setState({ image: uri });
+}
+      console.log('The image is' + this.state.image)
     };
 
-
-signUpUser = (name, major, email, password, passwordConfirm) => {
-
-
-
+signUpUser = (name, major, email, password, passwordConfirm, callback) => {
       auth.createUserWithEmailAndPassword(email, password)
-      .then(function(data){
-        console.log('created')
-
+      .then((data)=>{
+        console.log(data.user.uid)
         database.ref('users/' + data.user.uid).set({
         name: name,
         major: major,
@@ -66,12 +54,37 @@ signUpUser = (name, major, email, password, passwordConfirm) => {
         password: password,
         });
 
-    //Routing to registration screen
-
+        this.setState({currentID: data.user.uid})
+        console.log('ID IS: ' + this.state.currentID)
+        this.uploadImage()
+        console.log('Account Created')
     })
 
-
 }
+
+uploadImage = async (uri, imageName) => {
+  // Create file metadata including the content type
+  var metadata = {
+    contentType: 'image/jpeg',
+  }
+
+  var firebaseStorageRef = firebase.storage().ref(ref);
+     const imageRef = firebaseStorageRef.child(imageName + ".jpeg");
+
+     LOG.debug("FirebaseStorageService :: imageRef ", {imageRef:imageRef});
+
+
+     imageRef.putFile(uri, {contentType: 'image/jpeg'}).then(function(){
+         return imageRef.getDownloadURL();
+     }).then(function(url){
+         LOG.debug("Image url", {url:url});
+         onSuccess(url);
+     }).catch(function(error){
+         LOG.error("Error while saving the image.. ", error);
+         onError(error);
+     });
+}
+
 
   render() {
     return (
@@ -137,7 +150,7 @@ signUpUser = (name, major, email, password, passwordConfirm) => {
       />
       <TouchableOpacity
       style={styles.ButtonContainer}
-      onPress={() => this.signUpUser(this.state.name, this.state.major, this.state.email, this.state.password,this.state.passwordConfirm)}
+      onPress={() => this.signUpUser(this.state.name, this.state.major, this.state.email, this.state.password,this.state.passwordConfirm, this.uploadImage)}
       >
         <Text style = {styles.ButtonText}>SIGN UP</Text>
       </TouchableOpacity>
@@ -151,13 +164,11 @@ signUpUser = (name, major, email, password, passwordConfirm) => {
   }
 }
 
-
 const styles = StyleSheet.create({
   container: {
     padding: 20,
     backgroundColor: 'rgb(120,47,65)',
     paddingBottom: 100
-
   },
   input:{
     height: 40,
